312f01d7718d5eac1c9bc2792d9a74d045d2c18e
Rajkumar prasad <rajkumarprasadkth@gmail.com>
2025-08-29 15:31:29 +0530

    Initial commit - Spring Boot Simple Tour Package API setup

---
+++ src/main/java/com/backendapi/simpletourpackageapi/SimpleTourPackageApiApplication.java
@@ -0,0 +1,13 @@
+package com.backendapi.simpletourpackageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTourPackageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTourPackageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendapi/simpletourpackageapi/SimpleTourPackageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendapi.simpletourpackageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTourPackageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
